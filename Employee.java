cb25cb38c26ada1bda5ecff2d4c5e7b6600c9406
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2023-04-10 22:23:03 +0530

    master

--- Employee.java
+++ Employee.java
@@ -1,70 +1,63 @@
-/*
-
-WAP  to compute the tax according to the givedn codnition in the table 02 and display the output
-inputInfo();
-computeTax();
-displayInfo();
-
-tax:
-upto 250000 no tax
-upto 250000 to 300000 10% of the income excedding 2,50,000
-upto 300000 to 400000 5000+20% of the income excedding 3,00,000
-above 400000 25000+30% of the income exceeding 4,00,000
-member: 
-name: to store the name of the employee
-income : to store the income of the employee
-tax : to store the tax amount
-panNo: to store the pan number of the employee
-*/
-//java code 
- 
-import java.util.Scanner;
-
-class Employee{
-    String name;
-     int income;
-    float tax;
-    String panNo;
-    Employee(){}
-    void inputInfo(){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the name of the employee: ");
-        name=sc.nextLine();
-        System.out.print("Enter the income of the employee: ");
-        income=sc.nextInt();
-        System.out.print("Enter the pan number of the employee: ");
-        panNo=sc.next();
+//Write a class for following scenerio
+//The employee have four attributes name, id, salary, company name
+//The employee have list of methods
+// void setId(int id)
+// void setName(String name)
+// void setSalary(int salary)
+// void setCompanyName(String companyName)
+// int getId()
+// String getName()
+// int getSalary()
+//if employee are considered from same , then use an efficient memory management technique to handle company name
+public class Employee {
+     int id;
+   String name;
+     int salary;
+   static String companyName;
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+     void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public String getCompanyName(){
+        return companyName;
+    }
+    public static void main(String args[]){
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.setId(1);
+        e1.setName("Rahul");
+        e1.setSalary(10000);
+        e1.setCompanyName("Google");
+        e2.setId(2);
+        e2.setName("Rohit");
+        e2.setSalary(20000);
+        System.out.println("The id of e1 is: "+e1.getId());
+        System.out.println("The name of e1 is: "+e1.getName());
+        System.out.println("The salary of e1 is: "+e1.getSalary());
+        System.out.println("The company name of e1 is: "+e1.getCompanyName());
+        System.out.println("The id of e2 is: "+e2.getId());
+        System.out.println("The name of e2 is: "+e2.getName());
+        System.out.println("The salary of e2 is: "+e2.getSalary());
+        System.out.println("The company name of e2 is: "+e2.getCompanyName());
     }
 
-void computeTax(){
-    if(income<=250000){
-        tax=0;
-    }
-    else if(income>250000 && income<=300000){
-        tax=0.1f*(income-250000);
-    }
-    else if(income>300000 && income<=400000){
-        tax=5000+0.2f*(income-300000);
-    }
-    else{
-        tax=25000+0.3f*(income-400000);
-    }
+    
 }
-     void displayInfo(){
-        System.out.println("---------------------------------------------------");
-        System.out.println("Name of the employee: "+name);
-        System.out.println("Income of the employee: "+income);
-        System.out.println("Tax amount of the employee: "+tax);
-        System.out.println("Pan number of the employee: "+panNo);
-        System.out.println("---------------------------------------------------");
-    }
-
-    public static void main(String[] args) {
-        Employee e=new Employee();
-        e.inputInfo();
-        e.computeTax();
-        e.displayInfo();
-    }
-} 
-
 
---
+++ tax.java
@@ -0,0 +1,70 @@
+/*
+
+WAP  to compute the tax according to the givedn codnition in the table 02 and display the output
+inputInfo();
+computeTax();
+displayInfo();
+
+tax:
+upto 250000 no tax
+upto 250000 to 300000 10% of the income excedding 2,50,000
+upto 300000 to 400000 5000+20% of the income excedding 3,00,000
+above 400000 25000+30% of the income exceeding 4,00,000
+member: 
+name: to store the name of the employee
+income : to store the income of the employee
+tax : to store the tax amount
+panNo: to store the pan number of the employee
+*/
+//java code 
+ 
+import java.util.Scanner;
+
+class tax{
+    String name;
+     int income;
+    float tax;
+    String panNo;
+    tax(){}
+    void inputInfo(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the name of the employee: ");
+        name=sc.nextLine();
+        System.out.print("Enter the income of the employee: ");
+        income=sc.nextInt();
+        System.out.print("Enter the pan number of the employee: ");
+        panNo=sc.next();
+    }
+
+void computeTax(){
+    if(income<=250000){
+        tax=0;
+    }
+    else if(income>250000 && income<=300000){
+        tax=0.1f*(income-250000);
+    }
+    else if(income>300000 && income<=400000){
+        tax=5000+0.2f*(income-300000);
+    }
+    else{
+        tax=25000+0.3f*(income-400000);
+    }
+}
+     void displayInfo(){
+        System.out.println("---------------------------------------------------");
+        System.out.println("Name of the employee: "+name);
+        System.out.println("Income of the employee: "+income);
+        System.out.println("Tax amount of the employee: "+tax);
+        System.out.println("Pan number of the employee: "+panNo);
+        System.out.println("---------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        tax e=new tax();
+        e.inputInfo();
+        e.computeTax();
+        e.displayInfo();
+    }
+} 
+
+

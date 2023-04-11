54dbd1340a205786eb5d06e08ba6d1157a4c3afb
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2023-04-11 10:28:28 +0530

    master

--- SuperFunc.java
+++ SuperFunc.java
@@ -1,11 +1,20 @@
 //Write a program to demonstrate the use of super keyword in java.
+class A {   
 
-class A {
+    A(){
+        System.out.println("This is the constructor of the A class");
+    }
     int i = 10;
 }
 
 class B extends A {
+    B(){
+        super();
+        System.out.println("This is the constructor of the B class");
+    }
+   
     int i = 20;
+
     void display() {
         System.out.println("Value of i in class A is: " + super.i);
         System.out.println("Value of i in class B is: " + i);
